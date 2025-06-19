1c9dd05d5af1a7146e00f287b3adaaf28e92696e
nipunanoob <nipunanoob@gmail.com>
2025-06-19 19:11:07 +0530

    Add user input for calling llm query

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
+import sys
 
 load_dotenv()
+
+if len(sys.argv) == 1:
+    raise ValueError("Please add a string argument when calling this function for generating gemeni output")
+
+query = sys.argv[1]
+
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-
-
 response = client.models.generate_content(model='gemini-2.0-flash-001',
-                                          contents="Why is Boot.dev such a great place to learn backend " \
-                                          "development? Use one paragraph maximum.")
+                                          contents=query)
 
 print(response.text)
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
