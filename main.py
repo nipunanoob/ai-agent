a56e27588700154bd22aef5ed282bbd823da7097
nipunanoob <nipunanoob@gmail.com>
2025-06-21 13:43:28 +0530

    Add optional verbose flag

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
+import argparse
 
-load_dotenv()
+def main():
 
-if len(sys.argv) == 1:
-    raise ValueError("Please add a string argument when calling this function for generating gemeni output")
+    if len(sys.argv) == 1:
+        raise ValueError("Please add a string argument when calling this function for generating gemeni output")
 
-query = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("user_prompt", help="query which is going to be passed to Gemeni API")
+    parser.add_argument("--verbose", help="increase verbosity of output", action="store_true")
+    args = parser.parse_args()
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)]),
+    ]
 
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(model='gemini-2.0-flash-001',
-                                          contents=query)
+    response = client.models.generate_content(model='gemini-2.0-flash-001',
+                                            contents=messages)
 
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
