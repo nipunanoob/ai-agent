31fddacdaeab9536f23b50fecb56e1ce46b18049
nipunanoob <nipunanoob@gmail.com>
2025-07-01 21:39:35 +0530

    Write schema for function and get llm to call the function based on user query

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from google import genai
 from google.genai import types
 import sys
 import argparse
-
+from functions.get_files_info import schema_get_files_info
 
 
 def main():
@@ -12,7 +12,6 @@ def main():
     if len(sys.argv) == 1:
         raise ValueError("Please add a string argument when calling this function for generating gemini output")
 
-    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
 
     parser = argparse.ArgumentParser()
     parser.add_argument("user_prompt", help="query which is going to be passed to Gemini API")
@@ -26,11 +25,35 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+    available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+    )
+
+
     response = client.models.generate_content(model='gemini-2.0-flash-001',
                                             contents=messages,
-                                            config=types.GenerateContentConfig(system_instruction=system_prompt))
+                                            config=types.GenerateContentConfig(
+                                                tools=[available_functions], system_instruction=system_prompt
+                                            ))
 
-    print(response.text)
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f"Response: {response.text}")
+
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
